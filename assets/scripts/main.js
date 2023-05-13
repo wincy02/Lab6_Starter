2a0f57903f5ff4fdafd349c6521fb9ddb64b4290
wincy02 <yol050@ucsd.edu>
2023-05-13 11:18:06 -0700

    task completed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return JSON.parse(localStorage.getItem('recipes'));
 }
 
 /**
@@ -39,6 +40,13 @@ function addRecipesToDocument(recipes) {
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  const main = document.querySelector('main');
+  for (const element of recipes){
+    const card=document.createElement('recipe-card');
+    card.data=element;
+    main.appendChild(card);
+  }
+
 }
 
 /**
